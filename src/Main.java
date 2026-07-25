b03e3c6aed7d46d71962a0979c38f10d2df0d506
StickManM <stickmanm14@gmail.com>
2026-07-25 17:05:10 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import processing.core.PApplet;
+
+
+public class Main extends PApplet {
+    int[][] board;
+    int columnCount, rowCount;
+    int scale;
+
+    public void settings() {
+        size(900,1000);
+        pixelDensity(1);
+    }
+
+    public void setup() {
+        scale = 5;
+        rowCount = height/5;
+        columnCount = width/5;
+        board = new int [rowCount][columnCount];
+
+        for (int r = 0; r < rowCount; r++) {
+            for (int c = 0; c < columnCount; c++) {
+                board[r][c] = (int) random(2);
+            }
+        }
+        noStroke();
+    }
+
+    public void draw() {
+        background(0);
+        for (int r = 0; r < rowCount; r++) {
+            for (int c = 0; c < columnCount; c++) {
+                fill(255*board[r][c]);
+                rect(c*scale,r*scale,scale,scale);
+            }
+        }
+        update();
+    }
+
+    void update() {
+        int[][] tempBoard = new int[rowCount][columnCount];
+        int neighbors;
+        for (int r = 0; r < rowCount; r++) {
+            for (int c = 0; c < columnCount; c++) {
+                neighbors = countNeighbors(r,c);
+                if (neighbors == 3) {
+                    tempBoard[r][c] = 1;
+                } else if (neighbors == 2) {
+                    tempBoard[r][c] = board[r][c];
+                } else {
+                    tempBoard[r][c] = 0;
+                }
+            }
+        }
+        for (int r = 0; r < rowCount; r++) {
+            for (int c = 0; c < columnCount; c++) {
+                board[r][c] = tempBoard[r][c];
+            }
+        }
+    }
+
+    int countNeighbors(int r, int c) {
+        int value = 0;
+        int range = 1;
+        int neighborR, neighborC;
+
+        for (int dr = -range; dr <= range; dr++) {
+            for (int dc = -range; dc <= range; dc++) {
+                if (dr != 0|| dc != 0) {
+                    neighborR = ((r + dr) + rowCount) % rowCount;
+                    neighborC = ((c + dc) + columnCount) % columnCount;
+                    value += board[neighborR][neighborC];
+
+                }
+            }
+        }
+        return value;
+    }
+
+    public static void main(String[] passedArgs) {
+        PApplet.main(new String[] {Main.class.getName() });
+    }
+}
